2ff99a57da04baafaacbb0a4ed3a68e9741453cf
PRIYADHARSHINI <151350953+Priya-350@users.noreply.github.com>
2024-12-20 19:43:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,14 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import "dotenv/config"
 import userRoutes from "./routes/UserRoutes.js"
-await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`).then(()=>{
+await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
+  .then(() => {
     console.log("mongodb is connected..")
-})
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
 const PORT=process.env.PORT||4000
 const app=express()
 app.use(express.json())
